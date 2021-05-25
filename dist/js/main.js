9e6c2c53716e48b5ca1b5e235a27df21638aba63
Momobilalribery <elnenotorres48@yahoo.com>
2021-05-25 04:02:03 +0200

    Lesson 12, 13, 14

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,15 @@
-$(() => {
-    'use strict';
-    $('.toggle-sidebar').on('click', () => {
-        $('.content-area, .sidebar').toggleClass('no-sidebar');
-    });
+$(function() {
+  'use strict';
+  $('.toggle-sidebar').on('click', function() {
+      $('.content-area, .sidebar').toggleClass('no-sidebar');
+  });
+  // Toggle submenu
+  $('.toggle-submenu').on('click', function() {
+      $(this)
+      .find(".fa-angle-right")
+      .toggleClass("down");
+      $(this)
+      .next('.child-links')
+      .slideToggle();
+  });
 });
